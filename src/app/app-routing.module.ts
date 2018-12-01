4a42b8a14de6f6c6ed5f6680f9ae95b64c2d1fc3
sutin1234 <tony.sutin1234@gmail.com>
2018-12-01 22:58:46 +0700

    add more routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { IndexComponent } from './components/index/index.component';
+import { FilenotfoundComponent } from './components/filenotfound/filenotfound.component';
 
 
 const routes: Routes = [
+  { path: '', component: IndexComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: FilenotfoundComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { IndexComponent } from './components/index/index.component';
+import { FilenotfoundComponent } from './components/filenotfound/filenotfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    IndexComponent,
+    FilenotfoundComponent
   ],
   imports: [
     BrowserModule,
